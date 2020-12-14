d5cd2f4ded488b98aadc0331b127ee598d63afe0
steffakasid <github@steffen-rumpf.de>
2020-12-14 22:39:29 +0100

    Added base installation

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM alpine:3.12
 
+USER root
 # Install Go
 ARG GO_VERSION="1.15.6"
 ARG USERNAME="vscode"
     
 # Copy setup.sh and run it
 COPY setup.sh /opt/setup.sh
-RUN /opt/setup.sh
+RUN /opt/setup.sh ${GO_VERSION} ${USERNAME}
--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+set -e
+
+GO_VERSION=${1:-"1.15.6"}
+USERNAME=${2:-"vscode"}
+
+# Create User
+addgroup --gid 1000 $USERNAME
+adduser -s /bin/zsh -u 1000 -D -G $USERNAME $USERNAME
+
 # Install some additional packages
 apk update
-apk add --no-cache zsh=~5.8-r1
+apk add --no-cache zsh=~5.8-r1 git
 
-apk add --no-cache --virtual .build-deps curl git gcc musl-dev openssl go
+apk add --no-cache --virtual .build-deps bash curl gcc musl-dev openssl go
 
 curl https://dl.google.com/go/go${GO_VERSION}.src.tar.gz --output go.src.tar.gz
 tar -C /usr/local -xzf go.src.tar.gz
@@ -17,3 +26,6 @@ sh ./install.sh &&
     
 # cleanup build-deps
 apk del .build-deps
+
+cp -r /root/.oh-my-zsh /home/vscode/.oh-my-zsh
+chown -R $USERNAME:$USERNAME /home/vscode/.oh-my-zsh
